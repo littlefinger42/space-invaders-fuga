89723035dfd00328b4b67f268fd2ccf63473cb7f
Laurie Tendulkar <lauriemail@protonmail.com>
2019-06-21 13:54:51 +0200

    Hardcoded second example space invader

--- index.js
+++ index.js
@@ -7,18 +7,29 @@ const PatternFinder = require('./patternFinder.js');
 const args = process.argv.slice(2)
 
 const shape = [
-    ["-","-","-","o","o","-","-","-"],
-    ["-","-","o","o","o","o","-","-"],
-    ["-","o","o","o","o","o","o","-"],
-    ["o","o","-","o","o","-","o","o"],
-    ["o","o","o","o","o","o","o","o"],
-    ["-","-","o","-","-","o","-","-"],
-    ["-","o","-","o","o","-","o","-"],
-    ["o","-","o","-","-","o","-","o"]
+    "---oo---",
+    "--oooo--",
+    "-oooooo-",
+    "oo-oo-oo",
+    "oooooooo",
+    "--o--o--",
+    "-o-oo-o-",
+    "o-o--o-o"
+]
+
+const shape2 = [
+    "--o-----o--",
+    "---o---o---",
+    "--ooooooo--",
+    "-oo-ooo-oo-",
+    "ooooooooooo",
+    "o-ooooooo-o",
+    "o-o-----o-o",
+    "---oo-oo---"
 ]
 
 if (checkArguements(args)) {
-    PatternFinder.init(args).then(PatternFinder => PatternFinder.detectShapes(shape));
+    PatternFinder.init(args).then(PatternFinder => PatternFinder.detectShapes(shape2));
 } else {
     return remindSyntax();
 }
@@ -27,6 +38,7 @@ function remindSyntax() {
     console.warn(chalkWarning('Syntax "node index.js [BG_CHAR] [FG_CHAR] [PATH]"'))
     console.warn(chalkWarning('Example: "node index.js - 0 ./image.txt"'))
 }
+
 function checkArguements(args) {
     if (args.length !== 3) return console.error(chalkError('ERROR: Must supply 3 arguments. Currently supplied %s'), args.length)
     if (args[0].length !== 1) return console.error(chalkError('Error: First arguement must be 1 character. Currently supplied %s'), args[0])
