197cb395cf2fde8fd175ff3bfbb5732780a75775
Laurie Tendulkar <lauriemail@protonmail.com>
2019-06-24 12:44:48 +0200

    Added doc-blocks

--- patternFinder.js
+++ patternFinder.js
@@ -6,11 +6,22 @@ module.exports = class PatternFinder {
         this.map = mapArray;
         this.shape = shapeArray;
     }
+    /**
+     * Builds the pattern finder aftr waiting for async operations
+     * @param {string} mapPath 
+     * @param {string} shapePath 
+     * @returns {PatternFinder}
+     */
     static async init(mapPath, shapePath) {
         const mapArray = await this.getArrayFromTxt(mapPath);
         const shapeArray = await this.getArrayFromTxt(shapePath);
         return new PatternFinder(mapArray, shapeArray)
     }
+    /**
+     * Helper function which retrieves array from 
+     * @param {string} mapPath 
+     * @returns {Array.<string>|Object}
+     */
     static getArrayFromTxt(mapPath) {
         return new Promise((resolve, reject) => {
             fs.readFile(mapPath, 'utf8', (err, map) => {
@@ -21,6 +32,9 @@ module.exports = class PatternFinder {
             });
         })
     }
+    /**
+     * @returns {Object.<string>}
+     */
     detectShapes() {
         const heightSearchLimit = this.map.length - this.shape.length + 1;
 
